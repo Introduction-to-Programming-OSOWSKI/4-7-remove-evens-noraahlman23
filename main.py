92b1422fe0d63792f7a6899009b24947fec2330d
noraahlman23 <819446@mystma.org>
2021-02-23 16:07:55 +0000

    remove evens

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    potato = k
+    numPopped = 0
+    for i in range (0, len(potato)-1):
+        if potato[i - numPopped] % 2  == 0:
+            potato.pop(i - numPopped)
+            numPopped = numPopped + 1
+
+    return potato
+
+print (removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
